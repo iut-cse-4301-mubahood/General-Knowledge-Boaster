e5e64f174c940bcca4089168bee304a5c7912daa
Kaal <37835530+kaal321@users.noreply.github.com>
2018-05-08 00:51:57 +0600

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -1,3 +1,3 @@
 //All classes to be used in the project are saved here
 //I think since this is an external file, then it should be changed to .h but not .cpp
-//It's to notice us meeting day before 3 or 2 day atleast.
+//It's better to notice us meeting day before 3 or 2 day atleast.
