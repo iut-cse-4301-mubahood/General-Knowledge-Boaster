104deb80f3e80dacf94fd77b42933f5c91011ef7
Kaal <37835530+kaal321@users.noreply.github.com>
2018-05-08 00:51:25 +0600

    Next meeting day
    Yeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaah

--- classes.cpp
+++ classes.cpp
@@ -1,2 +1,3 @@
 //All classes to be used in the project are saved here
 //I think since this is an external file, then it should be changed to .h but not .cpp
+//It's to notice us meeting day before 3 or 2 day atleast.
